9ef88d66fd374818007710a9d2af50e9ac56e05e
Alexandr Chaika <unfii@ukr.net>
2019-08-14 12:07:14 +0300

    add script for python2 and python3

--- pip-install-upgrade-packages-python3.sh
+++ pip-install-upgrade-packages-python3.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 touch /usr/bin/anaconda/envs/py35/lib/python3.5/site-packages/easy-install.pth
 /usr/bin/anaconda/envs/py35/bin/pip install --upgrade pip $@
-/usr/bin/anaconda/bin/pip install --upgrade pip $@
