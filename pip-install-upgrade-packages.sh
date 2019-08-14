2db867fb5b182af26fb0e92d68bacb0431a9d3a0
Alexandr Chaika <unfii@ukr.net>
2019-08-14 11:34:05 +0300

    install also for python 2

--- pip-install-upgrade-packages.sh
+++ pip-install-upgrade-packages.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 touch /usr/bin/anaconda/envs/py35/lib/python3.5/site-packages/easy-install.pth
 /usr/bin/anaconda/envs/py35/bin/pip install --upgrade pip $@
+/usr/bin/anaconda/bin/pip install --upgrade pip $@
