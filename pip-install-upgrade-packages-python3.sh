83d72d0e6bf2c7bc106d00f40c0d424252f3aff2
Alexandr Chaika <unfii@ukr.net>
2019-08-16 05:50:46 +0300

    add download python module

--- pip-install-upgrade-packages-python3.sh
+++ pip-install-upgrade-packages-python3.sh
@@ -2,3 +2,4 @@
 touch /usr/bin/anaconda/envs/py35/lib/python3.5/site-packages/easy-install.pth
 /usr/bin/anaconda/envs/py35/bin/pip install --upgrade pip $@
 /usr/bin/anaconda/envs/py35/bin/python -m spacy download en_core_web_sm
+/usr/bin/anaconda/envs/py35/bin/python -m nltk.downloader stopwords
