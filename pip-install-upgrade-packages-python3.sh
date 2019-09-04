e6ce6d32f9b84f8aefc14d8706a70f991c1584b4
Alexandr Chaika <unfii@ukr.net>
2019-09-04 11:42:34 +0300

    upgrade python3 packages script install

--- pip-install-upgrade-packages-python3.sh
+++ pip-install-upgrade-packages-python3.sh
@@ -3,3 +3,6 @@ touch /usr/bin/anaconda/envs/py35/lib/python3.5/site-packages/easy-install.pth
 /usr/bin/anaconda/envs/py35/bin/pip install --upgrade pip $@
 /usr/bin/anaconda/envs/py35/bin/python -m spacy download en_core_web_sm
 /usr/bin/anaconda/envs/py35/bin/python -m nltk.downloader -d /usr/share/nltk_data all
+sudo apt-get purge -y python3-pip
+sudo apt-get install -y python3-pip
+sudo pip3 install $@
